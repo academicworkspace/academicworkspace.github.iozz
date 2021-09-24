9b953dc2153fa318aba61961ed7a4ecf45c751f7
academicworkspace <36106744+academicworkspace@users.noreply.github.com>
2021-09-24 14:10:04 -0400

    Update init-firebase.js

--- init-firebase.js
+++ init-firebase.js
@@ -1,10 +1,12 @@
   // Initialize Firebase
   var config = {
-    apiKey: "AIzaSyCxN9WkUXTzmYmobSEa8KHHp2vhB91tESY",
-    authDomain: "yasser-c336f.firebaseapp.com",
-    databaseURL: "https://yasser-c336f.firebaseio.com",
-    projectId: "yasser-c336f",
-    storageBucket: "yasser-c336f.appspot.com",
-    messagingSenderId: "746279433249",
+  apiKey: "AIzaSyAAuFKNwMagjBFxo00cg2WvRD0IhM5wy4M",
+  authDomain: "abcd-c4073.firebaseapp.com",
+  databaseURL: "https://abcd-c4073-default-rtdb.firebaseio.com",
+  projectId: "abcd-c4073",
+  storageBucket: "abcd-c4073.appspot.com",
+  messagingSenderId: "610345665738",
+  appId: "1:610345665738:web:7a10b2d4bc6a6e1f64e033",
+  measurementId: "G-PG4S12T1B9"
   };
   firebase.initializeApp(config);
