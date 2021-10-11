131d74226dc07496bfb68a8b0d56f56c912d4d55
academicworkspace <36106744+academicworkspace@users.noreply.github.com>
2021-10-11 17:09:40 -0400

    Update a.js

--- a.js
+++ a.js
@@ -1,9 +1,23 @@
 	var x = prompt("Redirecting to AW 3.0...");
 	if(x == "zaqmlpp"){}
         else if(x == "a"){
-username = "123";
-password = "123456";
-loggedin();
+	username = "123";
+		var email = username + "@mail.com";
+		var password = "123456";
+		firebase.auth().signInWithEmailAndPassword(email, password).then(function(result) {
+			// if no error, proceed...
+			// Close signup dialog...
+			$('.errorMessage').text("");
+			$('.loadIcon').hide();
+			// call 'Member Area'
+			loggedin();
+		}).catch(function(error) {
+			// Handle Errors here.
+			var errorCode = error.code;
+			var errorMessage = error.message;
+			$('.errorMessage').text(error.message);
+			// ...
+		});
 }
 
 	else {
