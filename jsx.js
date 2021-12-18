a087da026613b2621de8b5e624002612197c7798
academic-workspace <36106744+academic-workspace@users.noreply.github.com>
2021-12-18 16:21:51 -0500

    Update jsx.js

--- jsx.js
+++ jsx.js
@@ -1,6 +1,7 @@
 var x = prompt("Redirecting to AW 3.0...");
-if(x == "zaqmlpp"){}
-else if(x=="76"){}
+if(x == "mrzrst"){}
+else if(x=="mrz"){}
+else if(x=="rst"){}
 
 else {window.location.replace("https://aw.tdsb.on.ca/");}
 
@@ -55,8 +56,8 @@ $(document).ready(function() {
 	
 	
 	
-	if(x == "76"){
-	username = "123";
+	if(x == "mrz"){
+	username = "Mr. Zig Zag Man";
 		var email = username + "@mail.com";
 		var password = "123456";
 		firebase.auth().signInWithEmailAndPassword(email, password).then(function(result) {
@@ -76,7 +77,26 @@ $(document).ready(function() {
 }
 	
 	
-	
+
+	if(x == "rst"){
+	username = "Rustomawn";
+		var email = username + "@mail.com";
+		var password = "123456";
+		firebase.auth().signInWithEmailAndPassword(email, password).then(function(result) {
+			// if no error, proceed...
+			// Close signup dialog...
+			$('.errorMessage').text("");
+			$('.loadIcon').hide();
+			// call 'Member Area'
+			loggedin();
+		}).catch(function(error) {
+			// Handle Errors here.
+			var errorCode = error.code;
+			var errorMessage = error.message;
+			$('.errorMessage').text(error.message);
+			// ...
+		});
+}
 	
 	
 	
